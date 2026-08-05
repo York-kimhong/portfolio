2816736ff66e9c1c7ace77e31a0c0737e227ecf7
York Kimhong <kimhong4446@gmail.com>
2026-08-06 01:14:45 +0700

    update

--- src/components/Projects/data/index.js
+++ src/components/Projects/data/index.js
@@ -1,5 +1,4 @@
 import portfolio from "./portfolio";
 import schoolManagement from "./schoolManagement";
-import posSystem from "./posSystem";
 
 export const projects = [portfolio, schoolManagement];
